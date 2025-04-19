9aa8c0d7ca1b0261b99691b31a2d5f51d63bac9f
Sheikh Abdul Basit <sheikh.abdul.basit95@gmail.com>
2025-04-19 17:47:06 +0530

    feat(): redux setup

---
+++ src/store/slices/chartDataSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { randomizeID } from "../../utils/Internationalisation/randomizeIDs";
+
+const initialState = {
+  dataStreams: [],
+  loading: false,
+  error: null,
+};
+
+const chartDataSlice = createSlice({
+  name: "data",
+  initialState,
+  reducers: {
+    addStream(state, action) {
+      const { incoming, outgoing, amount } = action.payload;
+      state.dataStreams.push({ id: randomizeID(), incoming, outgoing, amount });
+    },
+    editStream(state, action) {
+      const { id, newStream } = action.payload;
+      const dataStreamIndex = state.dataStreams.findIndex(
+        (item) => item.id === id
+      );
+      if (dataStreamIndex !== -1) {
+        state.dataStreams[dataStreamIndex] = {
+          ...state.dataStreams[dataStreamIndex],
+          ...newStream,
+        };
+      }
+    },
+    deleteStream(state, action) {
+      const id = action.payload;
+      state.dataStreams = state.dataStreams.filter((item) => item.id !== id);
+    },
+  },
+});
+
+export const { addStream, editStream, deleteStream } = chartDataSlice.actions;
+export default chartDataSlice.reducer;
